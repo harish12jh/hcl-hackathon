9eb4c1a7a76a91a07ea7beda42612760e42ad4bc
Harish Kumar K <harish.12jh@gmail.com>
2025-03-29 17:16:39 +0530

    Resolving conflicts

--- src/Components/Layout.jsx
+++ src/Components/Layout.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 import NavBar from "./NavBar";
 
-const Layout = (setData) => {
+const Layout = ({ setData }) => {
   return (
     <>
       <NavBar setData={(values) => setData(values)}>
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -68,7 +68,10 @@ const Card = ({ data }) => {
             <div class="mt-2 flex justify-center w-full relative mb-2">
               <button
                 type="button"
-                onClick={() => addtoCart(product)}
+                onClick={() => {
+                  setCount(count + 1);
+                  addtoCart(product);
+                }}
                 class="flex items-center px-5 py-2.5 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
               >
                 <svg
@@ -79,7 +82,8 @@ const Card = ({ data }) => {
                   viewBox="0 0 18 21"
                 >
                   <path d="M15 12a1 1 0 0 0 .962-.726l2-7A1 1 0 0 0 17 3H3.77L3.175.745A1 1 0 0 0 2.208 0H1a1 1 0 0 0 0 2h.438l.6 2.255v.019l2 7 .746 2.986A3 3 0 1 0 9 17a2.966 2.966 0 0 0-.184-1h2.368c-.118.32-.18.659-.184 1a3 3 0 1 0 3-3H6.78l-.5-2H15Z" />
-                </svg>{" "}
+                </svg>
+                {count + " "}
                 Add to Cart
                 <span class="flex items-center justify-center w-4 h-4 ms-2 text-xs font-semibold text-blue-800 bg-blue-200 rounded-full">
                   {count}
